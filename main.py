fa83466277030c083fc9ed54224f97a0d21ecd08
Jayden Hendrickse <Jayden.heberhendrickse@gmail.com>
2024-06-03 21:13:03 +0200

    got though most steps. added requirements file and hid venv folder

--- main.py
+++ main.py
@@ -1,28 +1,26 @@
 from datetime import datetime
 import pandas
+import random
 # 2. Check if today matches a birthday in the birthdays.csv
 # HINT 1: Create a tuple from today's month and day using datetime. e.g.
 today = (datetime.now().month, datetime.now().day)
-
 # HINT 2: Use pandas to read the birthdays.csv
+data = pandas.read_csv("birthdays.csv")
 
 # HINT 3: Use dictionary comprehension to create a dictionary from birthday.csv that is formated like this:
-# birthdays_dict = {
-#     (birthday_month, birthday_day): data_row
-# }
+birthdays_dict = { (data["month"], data["day"]): data_row for (index, data_row) in data.items }
 #Dictionary comprehension template for pandas DataFrame looks like this:
 # new_dict = {new_key: new_value for (index, data_row) in data.iterrows()}
-#e.g. if the birthdays.csv looked like this:
-# name,email,year,month,day
-# Angela,angela@email.com,1995,12,24
-#Then the birthdays_dict should look like this:
-# birthdays_dict = {
-#     (12, 24): Angela,angela@email.com,1995,12,24
-# }
 
 #HINT 4: Then you could compare and see if today's month/day tuple matches one of the keys in birthday_dict like this:
-# if (today_month, today_day) in birthdays_dict:
-
+if today in birthdays_dict:
+    birthday_baby = birthdays_dict[today]
+    letter_num = random.randint(1,3)
+    letter_path = f"./letter_templates/letter_{letter_num}.txt"
+    with open(letter_path) as letter_file:
+        contents = letter_file.read()
+        contents.replace("[NAME]", birthday_baby["name"])
+    
 # 3. If there is a match, pick a random letter (letter_1.txt/letter_2.txt/letter_3.txt) from letter_templates and replace the [NAME] with the person's actual name from birthdays.csv
 # HINT 1: Think about the relative file path to open each letter. 
 # HINT 2: Use the random module to get a number between 1-3 to pick a randome letter.
